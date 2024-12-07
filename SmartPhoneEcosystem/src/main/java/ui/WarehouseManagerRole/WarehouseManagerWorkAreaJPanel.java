1ac5d00bb01f9582e354d10cac26ae8d2fb3025d
ZY-coder633 <zhao.yang11@northeastern.edu>
2024-12-07 18:58:49 -0500

    update warehouse management ui

--- SmartPhoneEcosystem/src/main/java/ui/WarehouseManagerRole/WarehouseManagerWorkAreaJPanel.java
+++ SmartPhoneEcosystem/src/main/java/ui/WarehouseManagerRole/WarehouseManagerWorkAreaJPanel.java
@@ -8,6 +8,7 @@ import Ecosystem.EcoSystem;
 import Ecosystem.Enterprise.Enterprise;
 import Ecosystem.Organization.Organization;
 import Ecosystem.UserAccount.UserAccount;
+import java.awt.CardLayout;
 import javax.swing.JPanel;
 
 /**
@@ -15,12 +16,18 @@ import javax.swing.JPanel;
  * @author sunny
  */
 public class WarehouseManagerWorkAreaJPanel extends javax.swing.JPanel {
-
-    /**
-     * Creates new form WarehouseManagerWorkAreaJPanel
-     */
+    JPanel userProcessContainer;
+    EcoSystem ecosystem;
+    private Organization organization;
+    private Enterprise enterprise;
+    private UserAccount account;
     public WarehouseManagerWorkAreaJPanel(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem ecosystem) {
         initComponents();
+        this.userProcessContainer = userProcessContainer;
+        this.ecosystem = ecosystem;
+        this.account = account;
+        this.organization = organization;
+        this.enterprise = enterprise;
     }
 
 
@@ -33,19 +40,66 @@ public class WarehouseManagerWorkAreaJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnManageProductRequest = new javax.swing.JButton();
+        warehouseLabel = new javax.swing.JLabel();
+        valueLabel = new javax.swing.JLabel();
+
+        btnManageProductRequest.setText("Manage Product Request");
+        btnManageProductRequest.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageProductRequestActionPerformed(evt);
+            }
+        });
+
+        warehouseLabel.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        warehouseLabel.setText("Warehouse :");
+
+        valueLabel.setText("<value>");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(btnManageProductRequest, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(102, 102, 102)
+                        .addComponent(warehouseLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(30, 30, 30)
+                        .addComponent(valueLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(124, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(119, 119, 119)
+                        .addComponent(warehouseLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(129, 129, 129)
+                        .addComponent(valueLabel)))
+                .addGap(29, 29, 29)
+                .addComponent(btnManageProductRequest)
+                .addContainerGap(235, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnManageProductRequestActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageProductRequestActionPerformed
+        RequestPrepareProductsJPanel manageWorkRequestJPanel = new RequestPrepareProductsJPanel(userProcessContainer, account, organization, enterprise, ecosystem);
+        userProcessContainer.add("manageWorkRequestJPanel", manageWorkRequestJPanel);
+        CardLayout layout=(CardLayout)userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+
+    }//GEN-LAST:event_btnManageProductRequestActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnManageProductRequest;
+    private javax.swing.JLabel valueLabel;
+    private javax.swing.JLabel warehouseLabel;
     // End of variables declaration//GEN-END:variables
 }
